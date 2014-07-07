62b99ce5e833fc70c70c10175c1a2bd018031977
Petter Walbø Johnsgård <petter@dotgrid.no>
2014-07-08 00:51:45 +0200

    Fixes #1 - Creates Duplicate Entries

--- tasks/px_to_rem.js
+++ tasks/px_to_rem.js
@@ -13,79 +13,126 @@ var chalk = require('chalk');
 module.exports = function (grunt) {
 
     grunt.registerMultiTask('px_to_rem', 'Convert px to rem', function() {
-        
+
         // Options
         var options = this.options({
             base: 16,
             fallback: false
-        });
+        });    
 
         var pxToRem = function (css) {
             var postcss = require('postcss'),
                 processor = postcss(function (css) {
-                    css.eachDecl(function (decl, i) {
+                    css.eachRule( function (rule ,i) {
 
-                        // Values of 0 shouldn't have units specified.
-                        decl.value = decl.value.replace( /([!]?\d*\.?\d+)\s*(pxi|px|rem)/g, function($1) {
-                            if ( $1 === '0pxi' || $1 === '0px' || $1 === '!0px' || $1 === '0rem' ) {
-                                $1 = 0;
-                            }
-                            return $1;
-                        });
+                        // Find elements that has fallback allready
+                        var hasFallback = checkFallback( rule.decls );
 
-                        var value = decl.value;
-                       
-                        // Change !px || pxi values to px
-                        if ( value.match( /(!|pxi)/ ) ) {
-                            value = value.replace( '!', '' ).replace( 'pxi', 'px' );
-                            decl.value = value;
-                        }
+                        rule.eachDecl(function (decl, i) {
                         
-                        else {
-                            // If already rem, create fallback if options.fallback
-                            if ( options.fallback && value.indexOf( 'rem' ) !== -1 ) {
-                                    value = value.replace( /(\d*\.?\d+)rem/ig, function ($1, $2) {
-                                        return Math.floor( parseFloat($2) * options.base ) + 'px';
+
+                            // Values of 0 shouldn't have units specified.
+                            decl.value = decl.value.replace( /([!]?\d*\.?\d+)\s*(pxi|px|rem)/g, function($1) {
+                                if ( $1 === '0pxi' || $1 === '0px' || $1 === '!0px' || $1 === '0rem' ) {
+                                    $1 = 0;
+                                }
+                                return $1;
+                            });
+
+                            var value = decl.value;
+                           
+                            // Change !px || pxi values to px
+                            if ( value.match( /(!|pxi)/ ) ) {
+                                value = value.replace( '!', '' ).replace( 'pxi', 'px' );
+                                decl.value = value;
+                            }
+                            
+                            else {
+                                // If both rem and px is defined return
+                                if ( hasFallback.indexOf( decl.prop ) !== -1 ) {
+                                    return decl.value;
+                                }
+
+                                // If already rem, create fallback if options.fallback
+                                if ( options.fallback && value.indexOf( 'rem' ) !== -1 ) {
+                                        value = value.replace( /(\d*\.?\d+)rem/ig, function ($1, $2) {
+                                            return Math.floor( parseFloat($2) * options.base ) + 'px';
+                                        });
+
+                                        decl.parent.insertBefore(i, decl.clone({ value: value }));
+                                        decl.fallback = true;
+                                } 
+
+                                // Convert px to rem
+                                if ( value.indexOf( 'px' ) !== -1 ) {
+                                    value = value.replace( /(\d*\.?\d+)px/ig, function ($1, $2) {
+                                        return parseFloat( $2 / options.base ) + 'rem';
                                     });
 
-                                    decl.parent.insertBefore(i, decl.clone({ value: value }));
-                                    decl.fallback = true;
-                            } 
-
-                            // Convert px to rem
-                            if ( value.indexOf( 'px' ) !== -1 ) {
-                                value = value.replace( /(\d*\.?\d+)px/ig, function ($1, $2) {
-                                    return parseFloat( $2 / options.base ) + 'rem';
-                                });
-
-                                if ( options.fallback && ! decl.fallback ) {
-                                    decl.parent.insertBefore( i, decl.clone() );
-                                    decl.value = value;
-                                } else {
-                                    decl.value = value;
+                                    if ( options.fallback && ! decl.fallback ) {
+                                        decl.parent.insertBefore( i, decl.clone() );
+                                        decl.value = value;
+                                    } else {
+                                        decl.value = value;
+                                    }
                                 }
                             }
-                        }
+                        });
+                       
                     });
+                    
                 });
 
             return processor.process(css).css;
         };
 
+        // Function to check if rem and px exist
+        function checkFallback( obj ) {
+            var props = [],
+                hasFallback = [],
+                values = [];
+
+            for (var prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    var pro = obj[prop].prop;
+
+                    if ( props.indexOf( pro ) === -1 ) {
+
+                        props.push( pro );
+                        values.push({prop: pro, val:obj[prop].value});
+
+                    } else {
+                        var lookup = {};
+                        for (var i = 0, len = values.length; i < len; i++) {
+                            lookup[values[i].prop] = values[i].val;
+                        }
+
+                        if ( lookup[pro].search( 'px' ) === -1 &&  obj[prop].value.search( 'rem' ) === -1) {
+                             hasFallback.push( pro );
+                        } else if ( lookup[pro].search( 'rem' ) === -1 &&  obj[prop].value.search( 'px' ) === -1) {
+                             hasFallback.push( pro );
+                        }
+                    }
+                }
+            }
+
+            return hasFallback;
+        }
+
         // Iterate over all specified file groups.
-        this.files.forEach( function (f) {
+        this.files.forEach( function ( f ) {
             // Concat specified files.
-            var src = f.src.filter( function (filepath) {
+            var src = f.src.filter( function ( filepath ) {
             
                 // Warn on and remove invalid source files (if nonull was set).
-                if ( ! grunt.file.exists(filepath) ) {
+                if ( ! grunt.file.exists( filepath ) ) {
                     grunt.log.warn( 'Source file "' + filepath + '" not found.' );
                     return false;
                 } else {
                     return true;
                 }
 
-            }).map( function (filepath) {
+            }).map( function ( filepath ) {
                 // Read file source & convert to rem unit
                 var css = pxToRem( grunt.file.read( filepath ) );
                 return css;
@@ -95,7 +142,7 @@ module.exports = function (grunt) {
             grunt.file.write( f.dest, src );
 
             // Print a success message.
-            grunt.log.writeln('File ' + chalk.cyan(f.dest) + ' created.');
+            grunt.log.writeln( 'File ' + chalk.cyan(f.dest) + ' created.' );
         });
     });
 };
